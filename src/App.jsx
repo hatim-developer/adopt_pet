d243aa0d57d06bb4934dcdb508b07346ec6294be
Mohammed Hatim <hatim.developer@gmail.com>
2024-09-02 23:00:37 +0530

    react, Jsx, and eslint setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Adopt Me!")
+  return (
+    <div>
+      <h1>Adapt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanas" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Boink" animal="cat" breed="Mix" />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
