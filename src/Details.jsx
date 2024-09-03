09d7fb7a406c8eaf642e1ef67921857936323644
Mohammed Hatim <hatim.developer@gmail.com>
2024-09-03 14:28:22 +0530

    React query - fetchPet api

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
 
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader"> 🌀 </h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
   return (
-    <div>
-      <h2>Hello: {id}</h2>
+    <div className="details">
+      <h1>{pet.name}</h1>
+      <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+      <button>Adopt {pet.name}</button>
+      <p>{pet.description}</p>
     </div>
   );
 };
